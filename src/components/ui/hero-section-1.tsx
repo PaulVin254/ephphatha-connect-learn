f7be255ed9d7c965145e5865039ca36b0c6bb562
Paul <vinwakolipaul@gmail.com>
2025-07-30 18:10:57 +0300

    Solved the issue where images stored in assets folder weren't rendering in the web page

--- src/components/ui/hero-section-1.tsx
+++ src/components/ui/hero-section-1.tsx
@@ -34,7 +34,7 @@ const formerStudents = [
     id: 1,
     name: "Paul Wakoli",
     designation: "KSL Interpreter",
-    image: "/src/assets/former-student-paul.jpg",
+    image: "/former-student-paul.jpg",
   },
   {
     id: 2,
@@ -236,14 +236,14 @@ export function HeroSection() {
                 <div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1">
                   <img
                     className="bg-background aspect-15/8 relative hidden rounded-2xl dark:block"
-                    src="/src/assets/sign-language-students.jpg"
+                    src="/sign-language-students.jpg"
                     alt="Sign language students learning"
                     width="2700"
                     height="1440"
                   />
                   <img
                     className="z-2 border-border/25 aspect-15/8 relative rounded-2xl border dark:hidden"
-                    src="/src/assets/sign-language-students.jpg"
+                    src="/sign-language-students.jpg"
                     alt="Sign language students learning"
                     width="2700"
                     height="1440"
